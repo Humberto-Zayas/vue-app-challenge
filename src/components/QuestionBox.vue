b2d565f6bc410c1eb4b983af7bef2922ddfd75af
Humberto Zayas <soundsinhd@gmail.com>
2022-01-26 18:26:23 -0500

    Init QuestionBox Component
    Created an initial empty question box component where the questions will live and imported to the Quiz view.

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>Question Box</h2>
+</template>
+
+<script>
+export default {
+  name: 'QuestionBox',
+}
+</script>
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -2,6 +2,7 @@
   <div class="home">
     
     <HelloWorld msg="Vue Quiz App Challenge"/>
+    <question-box />
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import dataset from '../assets/questions.json'
+import QuestionBox from '../components/QuestionBox.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    QuestionBox
   },
   data(){
     return {
